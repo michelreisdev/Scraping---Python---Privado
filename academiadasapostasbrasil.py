12a4fac302f96201bb2ca2ebe751bc887c3aab77
michelreisdev <81459995+michelreisdev@users.noreply.github.com>
2021-11-13 11:57:45 -0300

    academiadasapostasbrasil v1

--- academiadasapostasbrasil.py
+++ academiadasapostasbrasil.py
@@ -1,9 +1,9 @@
 import requests 
+from decimal import Decimal
 from bs4 import BeautifulSoup
 
-def pageEstatistica():
+def pageEstatistica(url):
     header = {'User_Agent':'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'}
-    url = 'https://www.academiadasapostasbrasil.com/stats/match/mundo/eliminatorias-para-a-copa-do-mundo-asia/vietna/japao/RaeQlPGkpQL6o/preview'
     page = requests.get(url, header)
     soup = BeautifulSoup( page.content, 'html.parser')
 
@@ -14,23 +14,38 @@ def pageEstatistica():
         if(len(a.string) > 1):
             times.append(a.text)
 
-    casaProbabilidade = soup.findAll("div","odd")[0]
-    casaProbabilidade = float( casaProbabilidade.a.text )
 
-    empateProbabilidade = soup.findAll("div","odd")[1]
-    empateProbabilidade = float(empateProbabilidade.a.text)
+    dataJogo = soup.findAll("li","gamehead")[1]
 
-    foraProbabilidade = soup.findAll("div","odd")[2]
-    foraProbabilidade = float( foraProbabilidade.a.text )
+    if(soup.findAll("div","preview_bet") == []):
+        return "%s Vs %s: Jogo Finalizado" % (times[0], times[2])
+    else:
+        tips = soup.findAll("div","preview_bet")[0]
+        tips = tips.p.text
+        return "%s \n%s Vs %s\n<em>%s</em> \n\n" % (dataJogo.text, times[0], times[2], tips)
+  
+def previews(url):
+    header = {'User_Agent':'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'}
+    page = requests.get(url, header)
+    soup = BeautifulSoup( page.content, 'html.parser')
+    a = []
+    for x in range(9):
+        tips = soup.findAll("div","preview_item list")[x]
+        tips = tips.findAll("a",href=True)[0]
+        a.append( pageEstatistica(tips['href']) )
+    return a
+print("_______________________________________________________________")
 
-    ganhador = times[1]
-    if( casaProbabilidade > foraProbabilidade ):
-        ganhador = times[2]
+pageInicial = int(5)
+pageFinal = int(5)
+arq = []
 
-    if( empateProbabilidade > foraProbabilidade or empateProbabilidade > casaProbabilidade):
-        ganhador = times[0]
+while pageFinal >= pageInicial:
+    url = f"https://www.academiadasapostasbrasil.com/previews/page/{pageInicial}"
+    arq.append( previews( url ))
+    pageInicial = pageInicial + 1
     
-    return "%s vs %s => %s" % (times[0], times[2], ganhador)
-
-    
-print( pageEstatistica() )
+with open("teste.txt","w") as arquivo:
+    for jogos in arq:
+        for jogos2 in jogos:
+            arquivo.write(jogos2)
