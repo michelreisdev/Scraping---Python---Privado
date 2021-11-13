870a9d66141b6c7c261c66d67ebf0454a3b98e4f
michelreisdev <81459995+michelreisdev@users.noreply.github.com>
2021-11-13 14:58:26 -0300

    academiadasapostasbrasil

--- academiadasapostasbrasil/academiadasapostasbrasil.py
+++ academiadasapostasbrasil/academiadasapostasbrasil.py
@@ -1,6 +1,15 @@
 import requests 
-from decimal import Decimal
 from bs4 import BeautifulSoup
+import configparser
+import os
+
+path = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
+config = configparser.ConfigParser()
+config.read(os.path.join(path, 'academiadasapostasbrasil.ini'))
+param = config["CONFIG"]
+pageList = param["pageList"]
+pageInicialInt = param["pageInicialInt"]
+pagefinalInt = param["pagefinalInt"]
 
 def pageEstatistica(url):
     header = {'User_Agent':'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'}
@@ -18,11 +27,11 @@ def pageEstatistica(url):
     dataJogo = soup.findAll("li","gamehead")[1]
 
     if(soup.findAll("div","preview_bet") == []):
-        return "%s Vs %s: Jogo Finalizado" % (times[0], times[2])
+        return "%s x %s: Jogo Finalizado" % (times[0], times[2])
     else:
         tips = soup.findAll("div","preview_bet")[0]
         tips = tips.p.text
-        return "%s \n%s Vs %s\n<em>%s</em> \n\n" % (dataJogo.text, times[0], times[2], tips)
+        return "%s \n%s x %s\n<em>%s</em> \n\n" % (dataJogo.text, times[0], times[2], tips)
   
 def previews(url):
     header = {'User_Agent':'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'}
@@ -36,8 +45,8 @@ def previews(url):
     return a
 print("_______________________________________________________________")
 
-pageInicial = int(5)
-pageFinal = int(5)
+pageInicial = int(pageInicialInt)
+pageFinal = int(pagefinalInt)
 arq = []
 
 while pageFinal >= pageInicial:
@@ -45,7 +54,8 @@ while pageFinal >= pageInicial:
     arq.append( previews( url ))
     pageInicial = pageInicial + 1
     
-with open("teste.txt","w") as arquivo:
+arq.append( "\n\nTotal: 35" )
+with open(os.path.join(path, 'preview.txt'),"w") as arquivo:
     for jogos in arq:
         for jogos2 in jogos:
             arquivo.write(jogos2)
