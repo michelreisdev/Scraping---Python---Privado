9e208aaa085b570fd4107c993fc6787fb31afdf8
michelreisdev <81459995+michelreisdev@users.noreply.github.com>
2021-11-14 12:14:50 -0300

    academiadasapostasbrasil

--- academiadasapostasbrasil/academiadasapostasbrasil.py
+++ academiadasapostasbrasil/academiadasapostasbrasil.py
@@ -2,60 +2,76 @@ import requests
 from bs4 import BeautifulSoup
 import configparser
 import os
-
-path = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
-config = configparser.ConfigParser()
-config.read(os.path.join(path, 'academiadasapostasbrasil.ini'))
-param = config["CONFIG"]
-pageList = param["pageList"]
-pageInicialInt = param["pageInicialInt"]
-pagefinalInt = param["pagefinalInt"]
-
-def pageEstatistica(url):
-    header = {'User_Agent':'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'}
-    page = requests.get(url, header)
-    soup = BeautifulSoup( page.content, 'html.parser')
-
-    times = []
-
-    """ times: [0] => time 01; [1] => 'vs'; [2] => time 02 """
-    for a in soup.findAll("div","stats-game-head-teamname")[2]:
-        if(len(a.string) > 1):
-            times.append(a.text)
+from progress.spinner import Spinner
 
 
-    dataJogo = soup.findAll("li","gamehead")[1]
+def previsoes():
+    path = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
+    config = configparser.ConfigParser()
+    config.read(os.path.join(path, 'academiadasapostasbrasil.ini'))
+    param = config["CONFIG"]
+    pageList = param["pageList"]
+    pageInicialInt = param["pageInicialInt"]
+    pagefinalInt = param["pagefinalInt"]
+    qtdPreview = int(param["qtdPreview"])
 
-    if(soup.findAll("div","preview_bet") == []):
-        return "%s x %s: Jogo Finalizado" % (times[0], times[2])
-    else:
-        tips = soup.findAll("div","preview_bet")[0]
-        tips = tips.p.text
-        return "%s \n%s x %s\n<em>%s</em> \n\n" % (dataJogo.text, times[0], times[2], tips)
-  
-def previews(url):
-    header = {'User_Agent':'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'}
-    page = requests.get(url, header)
-    soup = BeautifulSoup( page.content, 'html.parser')
-    a = []
-    for x in range(9):
-        tips = soup.findAll("div","preview_item list")[x]
-        tips = tips.findAll("a",href=True)[0]
-        a.append( pageEstatistica(tips['href']) )
-    return a
-print("_______________________________________________________________")
+    def pageEstatistica(url):
+        header = {'User_Agent':'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'}
+        page = requests.get(url, header)
+        soup = BeautifulSoup( page.content, 'html.parser')
 
-pageInicial = int(pageInicialInt)
-pageFinal = int(pagefinalInt)
-arq = []
+        times = []
 
-while pageFinal >= pageInicial:
-    url = f"https://www.academiadasapostasbrasil.com/previews/page/{pageInicial}"
-    arq.append( previews( url ))
-    pageInicial = pageInicial + 1
+        """ times: [0] => time 01; [1] => 'vs'; [2] => time 02 """
+        for a in soup.findAll("div","stats-game-head-teamname")[2]:
+            if(len(a.string) > 1):
+                times.append(a.text)
+
+
+        dataJogo = soup.findAll("li","gamehead")[1]
+
+        if(soup.findAll("div","preview_bet") == []):
+            return "%s x %s: Jogo Finalizado" % (times[0], times[2])
+        else:
+            tips = soup.findAll("div","preview_bet")[0]
+            tips = tips.p.text
+            return "%s \n%s x %s\n<em>%s</em> \n\n" % (dataJogo.text, times[0], times[2], tips)
     
-arq.append( "\n\nTotal: 35" )
-with open(os.path.join(path, 'preview.txt'),"w") as arquivo:
-    for jogos in arq:
-        for jogos2 in jogos:
-            arquivo.write(jogos2)
+    def previews(url, qtdPreview):
+        header = {'User_Agent':'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'}
+        page = requests.get(url, header)
+        soup = BeautifulSoup( page.content, 'html.parser')
+        a = []
+        for x in range(qtdPreview):
+            tips = soup.findAll("div","preview_item list")[x]
+            tips = tips.findAll("a",href=True)[0]
+            a.append( pageEstatistica(tips['href']) )
+        return a
+
+    pageInicial = int(pageInicialInt)
+    pageFinal = int(pagefinalInt)
+    arq = []
+
+    while pageFinal >= pageInicial:
+        url = f"https://www.academiadasapostasbrasil.com/previews/page/{pageInicial}"
+        arq.append( previews( url, qtdPreview ))
+        pageInicial = pageInicial + 1
+
+    total = "Total de Previews: %s" % (len(arq) * qtdPreview)
+
+    arq.append( total )
+
+    with open(os.path.join(path, 'preview.txt'),"w") as arquivo:
+        for jogos in arq:
+            for jogos2 in jogos:
+                arquivo.write(jogos2)
+    
+    return 1
+
+
+""" spinner = Spinner('Loading ')
+state = 0
+while state != 1:
+    spinner.next()
+    continue """
+previsoes()
