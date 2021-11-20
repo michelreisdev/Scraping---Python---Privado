94decd023527e0f389d471b9c7193e42adb89418
michelreisdev <81459995+michelreisdev@users.noreply.github.com>
2021-11-19 22:46:18 -0300

    academiadasapostasbrasil

--- academiadasapostasbrasil/academiadasapostasbrasil.py
+++ academiadasapostasbrasil/academiadasapostasbrasil.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import requests 
 from bs4 import BeautifulSoup
 import configparser
 import os
 from progress.spinner import PieSpinner
-from alive_progress import alive_bar
-import asyncio
-import tqdm
+
 
 def previsoes():
+    global contPreview
     spinner = PieSpinner('Buscando...')
     spinner.next()
     path = '/'.join((os.path.abspath(__file__).replace('\\', '/')).split('/')[:-1])
@@ -18,7 +19,7 @@ def previsoes():
     pageInicialInt = param["pageInicialInt"]
     pagefinalInt = param["pagefinalInt"]
     qtdPreview = int(param["qtdPreview"])
-
+    
     def pageEstatistica(url):
         spinner.next()
         header = {'User_Agent':'Mozilla/5.0 (Linux; Android 6.0.1; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Mobile Safari/537.36'}
@@ -37,7 +38,8 @@ def previsoes():
         dataJogo = soup.findAll("li","gamehead")[1]
 
         if(soup.findAll("div","preview_bet") == []):
-            return "%s x %s: Jogo Finalizado" % (times[0], times[2])
+            return ""
+            #return "%s x %s: Jogo Finalizado" % (times[0], times[2])
         else:
             tips = soup.findAll("div","preview_bet")[0]
             tips = tips.p.text
@@ -66,12 +68,20 @@ def previsoes():
         arq.append( previews( url, qtdPreview ))
         pageInicial = pageInicial + 1
         spinner.next()
-
-    total = "Total de Previews: %s" % (len(arq) * qtdPreview)
-
+    
+    cont = 0 
+    for jogos in arq:
+        spinner.next()
+        for jogos2 in jogos:
+            spinner.next()
+            if len(jogos2) > 10:
+                spinner.next()
+                cont = cont + 1
+    
+    total = "\n\nTotal de Previews: %s" % (cont)
     arq.append( total )
 
-    with open(os.path.join(path, 'preview.txt'),"w") as arquivo:
+    with open(os.path.join(path, 'preview.txt'),"w", encoding="utf-8") as arquivo:
         for jogos in arq:
             spinner.next()
             for jogos2 in jogos:
